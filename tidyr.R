71b242c46e5431650dd0362612df982862da104a
Yehor <egor.reznichenko17@gmail.com>
2020-12-07 01:26:21 +0200

    all functions done

--- tidyr.R
+++ tidyr.R
@@ -18,3 +18,50 @@ gathered_tibble <- gather(tibble, '1999', '2000',
                           key = "year", value = "cases")
 print(gathered_tibble)
 print(spread(gathered_tibble, year, cases))
+
+print ("__handle missing values____________")
+
+tibble <- tibble(x1 = sample(1:10, 3), x2 = c(1, NA, 5), x3 = c(4, NA, 10))
+print(tibble)
+print(drop_na(tibble))
+
+print(fill(tibble, x2, x3))
+
+print(replace_na(tibble, list(x2 = "new", x3 = "new")))
+
+print ("__expand tables____________________")
+
+tibble <- tibble(a = sample(1:3, 2), b = sample(1:3, 2), c = sample(1:3, 2),
+                 d = sample(1:3, 2), e = sample(1:3, 2))
+print(tibble)
+print(complete(tibble, a, b, c))
+print(expand(tibble, a, b, c))
+
+print ("__split cells______________________")
+tibble <- tibble(a = LETTERS[1:5], b = c("a/b", "b/c", "c/d", "d/e", "e/f"))
+
+print(tibble)
+print(separate(tibble, b, sep = "/", into = c("1", "2")))
+print(separate_rows(tibble, b, sep = "/"))
+tibble <- separate(tibble, b, sep = "/", into = c("1", "2"))
+print(unite(tibble, "1", "2", col = "b", sep = "/"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
