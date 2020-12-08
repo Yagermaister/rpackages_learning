9be0250496a92eb75ec6aa6a886add365a7eb270
Yehor <egor.reznichenko17@gmail.com>
2020-12-08 17:58:19 +0200

    have finished with readr pack

--- readr.R
+++ readr.R
@@ -1,4 +1,18 @@
-write_csv(data.frame(x1 = 1:3, x2 = letters[1:3], x3 = LETTERS[1:3]),
-          "data.csv")
+write_csv(data.frame(sample(1:3, 3), sample(1:3, 3), sample(1:3, 3)),
+          file = "data.csv")
 
+print("data read")
 print(read_csv("data.csv"))
+
+print("data read with headers x1, x2, x3 and skiping fist line")
+print(read_csv("data.csv", col_names=c("x1", "x2", "x3"), skip = 1))
+
+print("data read missing values '1'")
+print(read_csv("data.csv", col_names=c("x1", "x2", "x3"), skip = 1,
+               na = "1"))
+      
+print("data read parsing columns types")
+print(read_csv("data.csv", skip = 1, col_names=c("x1", "x2", "x3"),
+               col_types = cols(x1 = col_integer(),
+                                x2 = col_double(),
+                                x3 = col_character())))
